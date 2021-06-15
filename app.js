815f7638821a2d7b73b3009f89d6f5263ebad12d
bo-bi <18935445147@qq.com>
2021-06-15 10:41:20 +0800

    增加app和page的生命周期、转发页面

--- app.js
+++ app.js
@@ -1,5 +1,33 @@
 App({
-  onLoad() {
-    console.log("App onload");
-  }
+  onLoad () {
+    // Do something initial when load.
+    console.log('onLoad -- 监听小程序初始化');
+    // 可以拿到this
+  },
+
+  onShow () {
+    // Do something when show.
+    console.log('onShow -- 监听小程序启动或切前台');
+    // 拿不到this
+  },
+
+  onHide () {
+    // Do something when hide.
+    console.log('onHide -- 监听小程序切后台');
+    // 拿不到this
+  },
+
+  onUnload () {
+    // Do something when unload.
+    console.log('onUnload -- 监听小程序销毁');
+  },
+
+  onError (msg) {
+    console.log(`onError -- 错误监听函数: ${msg}`);
+  },
+
+  // 全局共享数据
+  globalData: {
+    name: '张三',
+  },
 })
--- pages/home/index.js
+++ pages/home/index.js
@@ -1,5 +1,39 @@
 Page({
-  onLoad() {
-    console.log("page loaded");
-  }
-});
+  onLoad: function () {
+    // Do some initialize when page load.
+    console.log('onLoad -- 监听页面加载')
+    setTimeout(() => {
+      this.updateData();
+    }, 3000);
+  },
+
+  onShow: function () {
+    // Do something when page show.
+    console.log('onShow -- 监听页面显示');
+  },
+
+  onReady: function () {
+    // Do something when page ready.
+    console.log('onReady -- 监听页面初次渲染完成');
+  },
+
+  onHide: function () {
+    // Do something when page hide.
+    console.log('onHide -- 监听页面隐藏');
+  },
+
+  onUnload: function () {
+    // Do something when page close.
+    console.log('onUnload -- 监听页面卸载');
+  },
+
+  onShareAppMessage: function () {
+    // 用户点击右上角转发
+    return {
+      title: '快来试下这是360er内测~',
+      // path: '/page/user?id=123',
+      path: '/page/mine/index?id=456',
+      imageUrl: 'http://p4.music.126.net/VDUjBvzZdMV8Hn_lYTt8fw==/109951165756214678.jpg'
+    }
+  },
+})
