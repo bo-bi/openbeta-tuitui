62bbdf7596e46874dce84fcb7907d7657ce43f7f
bo-bi <18935445147@qq.com>
2021-06-18 10:19:48 +0800

    增加vant@next UI库

--- postcss.config.js
+++ postcss.config.js
@@ -2,6 +2,11 @@ module.exports = {
   plugins: [
     require("postcss-px-to-viewport")({
       viewportWidth: 750,
-    })
+      exclude: [/node_modules/],
+    }),
+    require("postcss-px-to-viewport")({
+      viewportWidth: 750,
+      include: [/node_modules/],
+    }),
   ]
 };
