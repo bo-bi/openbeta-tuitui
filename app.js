bb70e966435bd1f9ffb912161b5d0afd8215fe90
bo-bi <18935445147@qq.com>
2021-08-31 19:57:03 +0800

    保证每次能获取到最新的头像

--- app.js
+++ app.js
@@ -5,6 +5,35 @@ App({
     // 可以拿到this
 
     // 此处便不进行小程序初始化登录了, 而且这里不好定位错误, 在每个页面需要登录的地方, 进行调用, 调用后再进行随后逻辑
+
+    // 在这里清除登录token, 保证每次获取最新的用户头像(只能每次调用qh.login, 服务端获取到的才能是最新的用户信息)
+    qh.removeStorage({
+      key: 'access_token_product',
+      success() {
+        // qh.showToast({
+        //   title: `移除 product 成功`,
+        // });
+      },
+      fail() {
+        // qh.showToast({
+        //   title: `移除 product 失败`,
+        // });
+      },
+    });
+
+    qh.removeStorage({
+      key: 'access_token_dev',
+      success() {
+        // qh.showToast({
+        //   title: `小程序销毁, 移除 access_token_dev 成功`,
+        // });
+      },
+      fail() {
+        // qh.showToast({
+        //   title: `小程序销毁, 移除 access_token_dev 失败`,
+        // });
+      },
+    });
   },
 
   onShow () {
@@ -25,6 +54,8 @@ App({
 
     // 在这里看下5分钟后小程序能不能销毁, 经测试, 是会清除的
     // 小程序销毁, 将本地存储的token移除(目前取消此操作, 保留登录状态一个礼拜, 不用再每次进入小程序重复登录, 但为了保证用户头像更新, 所以还是启用了, 有 5min 小程序自动销毁才会更新头像的时间差)
+
+    // 最终结论: 这里不保证走销毁的生命周期
     qh.removeStorage({
       key: 'access_token_product',
       success() {
